a94d01a85af87e90c40082890c5adfb2f4f9eab4
David Sveningsson <ext@sidvind.com>
2012-02-11 15:25:41 +0100

    moving yaml_error to common so it reusable

--- src/common.hpp
+++ src/common.hpp
@@ -2,5 +2,6 @@
 #define DVB021_COMMON_H
 
 const char* real_path(const char* filename);
+int yaml_error(struct yaml_parser_s* parser);
 
 #endif /* DVB021_COMMON_H */
--- src/level.cpp
+++ src/level.cpp
@@ -10,57 +10,6 @@
 #include <inttypes.h>
 #include <cassert>
 
-static int yaml_error(yaml_parser_t* parser){
-	switch (parser->error){
-	case YAML_MEMORY_ERROR:
-		fprintf(stderr, "Not enough memory for parsing\n");
-		break;
-
-	case YAML_READER_ERROR:
-		if (parser->problem_value != -1) {
-			fprintf(stderr, "Reader error: %s: #%X at %lu\n", parser->problem, parser->problem_value, parser->problem_offset);
-		} else {
-			fprintf(stderr, "Reader error: %s at %lu\n", parser->problem, parser->problem_offset);
-		}
-		break;
-
-	case YAML_SCANNER_ERROR:
-		if (parser->context) {
-			fprintf(stderr, "Scanner error: %s at line %lu, column %lu\n"
-			        "%s at line %lu, column %lu\n", parser->context,
-			        parser->context_mark.line+1, parser->context_mark.column+1,
-			        parser->problem, parser->problem_mark.line+1,
-			        parser->problem_mark.column+1);
-		} else {
-			fprintf(stderr, "Scanner error: %s at line %lu, column %lu\n",
-			        parser->problem, parser->problem_mark.line+1,
-			        parser->problem_mark.column+1);
-		}
-		break;
-
-	case YAML_PARSER_ERROR:
-		if (parser->context) {
-			fprintf(stderr, "Parser error: %s at line %lu, column %lu\n"
-			        "%s at line %lu, column %lu\n", parser->context,
-			        parser->context_mark.line+1, parser->context_mark.column+1,
-			        parser->problem, parser->problem_mark.line+1,
-			        parser->problem_mark.column+1);
-		} else {
-			fprintf(stderr, "Parser error: %s at line %lu, column %lu\n",
-			        parser->problem, parser->problem_mark.line+1,
-			        parser->problem_mark.column+1);
-		}
-		break;
-
-	default:
-		/* Couldn't happen. */
-		fprintf(stderr, "Internal error\n");
-		break;
-	}
-
-	exit(1);
-}
-
 class LevelPimpl {
 public:
 	LevelPimpl(const std::string& filename)
