525a4a6023e8f3f6e3f8d4f182f54f58854d8c91
David Sveningsson <ext@sidvind.com>
2012-02-27 14:24:17 +0100

    Adding msvc project (but still has issues with yaml having to be rebuilt every time)

---
+++ msvc/config.h
@@ -0,0 +1,15 @@
+/* config.h.  Generated manually for MSVC2011.  */
+
+#define DATA_DIR "."
+#define PACKAGE "frobnicator"
+#define PACKAGE_BUGREPORT "ext@sidvind.com"
+#define PACKAGE_NAME "frobnicator"
+#define PACKAGE_STRING "frobnicator 0.1"
+#define PACKAGE_TARNAME "frobnicator"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "0.1"
+#define VERSION "0.1"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+
+#include "forward.hpp"
